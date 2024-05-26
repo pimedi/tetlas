8c04569be3272cb49aa74fef78b88bb78dfeb48a
pimedi <166278430+pimedi@users.noreply.github.com>
2024-05-26 16:01:04 +0900

    Update bake_video.py

--- bake_video.py
+++ bake_video.py
@@ -1,11 +1,7 @@
 import cv2
 import os
 import re
-
-# 현재 작업 디렉토리 저장
-current_path = os.path.dirname(os.path.abspath(__file__))
-print(f"현재 디렉토리: {current_path}")
-
+import sys
 
 def natural_sort_key(s):
     return [int(text) if text.isdigit() else text.lower() for text in re.split('(\d+)', s)]
@@ -13,14 +9,14 @@ def natural_sort_key(s):
 def images_to_video(image_folder, output_video, fps):
     try:
         # Get a list of images in the folder
-        images = [img for img in os.listdir(image_folder) if img.endswith((".png", ".jpg", ".jpeg"))]
+        images = [img for img in os.listdir(image_folder) if img.endswith((".png", ".jpg", ".jpeg", ".png"))]
         images.sort(key=natural_sort_key)  # Ensure the images are sorted in natural order
 
         if not images:
             raise ValueError("No images found in the folder")
 
         # Read the first image to get dimensions
-        first_image_path = os.path.join(current_path, image_folder, images[0])
+        first_image_path = os.path.join(image_folder, images[0])
         frame = cv2.imread(first_image_path)
         if frame is None:
             raise ValueError(f"Could not read the first image: {first_image_path}")
@@ -44,17 +40,14 @@ def images_to_video(image_folder, output_video, fps):
 
     except Exception as e:
         print(f"Error: {e}")
-######################################################################################################
-################### 설정 Parameters###################################################################
-######################################################################################################
 
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python bake_video.py <image_folder> <output_video> <fps>")
+        sys.exit(1)
 
+    image_folder = sys.argv[1]
+    output_video = sys.argv[2]
+    fps = int(sys.argv[3])
 
-###이미지 폴더 이름을 붙여넣기 하세요. Copy and paste the image folder name.
-image_folder = os.path.join(current_path, 'Tetlas_-0.9371381382814938_-3.5086305438969156') 
-output_video = 'Tetlas_-0.9371381382814938_-3.5086305438969156.mp4'  # 출력할 동영상 파일 이름
-
-fps = 30  # 초당 프레임 수 (fps 설정)
-    
-# Convert images to video
-images_to_video(image_folder, output_video, fps)
+    images_to_video(image_folder, output_video, fps)
